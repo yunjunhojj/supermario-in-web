2fb2a4a0de1103e368e35a531e95534d3b8d2b27
Yunjunho <yunjunhojj@gmail.com>
2023-01-16 18:43:18 +0900

    player movement

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Player {
 
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y <= canvas.height)
@@ -36,11 +37,73 @@ class Player {
 }
 
 const player = new Player();
+const keys = {
+  right: {
+    pressed: false,
+  },
+  left: {
+    pressed: false,
+  },
+  up: {
+    pressed: false,
+  },
+  down: {
+    pressed: false,
+  },
+};
 
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
   player.update();
+
+  if (keys.right.pressed) {
+    player.velocity.x = 5;
+  } else if (keys.left.pressed) {
+    player.velocity.x = -5;
+  } else player.velocity.x = 0;
 }
 
 animate();
+
+addEventListener("keydown", ({ key }) => {
+  console.log("keydown", key);
+  switch (key) {
+    case "a":
+      console.log("move left");
+      keys.left.pressed = true;
+      break;
+    case "d":
+      console.log("move right");
+      keys.right.pressed = true;
+      break;
+    case "w":
+      console.log("move up");
+      player.velocity.y -= 10;
+      break;
+    case "s":
+      console.log("move down");
+      break;
+  }
+});
+
+addEventListener("keyup", ({ key }) => {
+  console.log("keyup 키업", key);
+  switch (key) {
+    case "a":
+      console.log("move left");
+      keys.left.pressed = false;
+
+      break;
+    case "d":
+      console.log("move right");
+      keys.right.pressed = false;
+      break;
+    case "w":
+      console.log("move up");
+      break;
+    case "s":
+      console.log("move down");
+      break;
+  }
+});
